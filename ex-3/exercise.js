73c6d802925137aeca86a4b97394f2d0f63bd34b
hassanwasswa <assanwasswa@gmail.com>
2021-02-26 17:09:56 +0100

    execrise3 finised

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,3 +1,16 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+var choices = [colors, presidents];
+var colors = ["red", "pink", "purple", "brown", "blue"];
+alert("My #1 choice is " + colors[2]);
+var presidents = ["Biden", "Hassan", "Trump", "Putin"];
+alert("My #1 choice is " + presidents[1]);
+
+const favourites = {
+    color: "Blue",
+    president: "Biden"
+}
+window.alert("My favourite color is " + favourites.color);
+alert("My favourite president is " + favourites.president);
