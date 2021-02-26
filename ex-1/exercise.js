b7c0860eb534e47fa8a38ef9697dc674d565561e
hassanwasswa <assanwasswa@gmail.com>
2021-02-26 12:07:50 +0100

    execrise1 objects and functions

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,3 +1,15 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+
+function user () {
+  var noun =prompt('Please insert in your noun');
+  var verb =prompt('Can your enter  the verb');
+  var adverb =prompt('can yout enter your adverb');
+  var adjective =prompt('can your enter the adjective'); 
+  var user = (' Hassan '+ noun + ' loves to travel ' + verb + '  and he loves ' + adverb + ' wearing black skinny jeans ' + adjective )
+  window.alert(user) 
+}
+
+user ()
