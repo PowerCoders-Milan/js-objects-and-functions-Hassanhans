b977dacc47788b81bd2293b584cb267508963093
hassanwasswa <assanwasswa@gmail.com>
2021-02-26 13:07:20 +0100

    execrise2 objects and functions

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,24 @@
 // write your JS here
 // remember to always test in the browser
-// to see if everything works correctly
+// to see if everything works correctl
+
+       var jobTitle =prompt('Please enter your desired jobtitle');
+        var City =prompt('Please enter your desired location');
+        var partner =prompt('Please enter your partner ');
+        var numkids =prompt('please enter the numkids');
+
+    function tellFortune(jobTitle, city, partner, numKids) {
+        this.jobTitle = jobTitle
+        this.geoLocation = city
+        this.partner = partner
+        this.numkids = numkids
+        window.alert('You will be a ' + jobTitle + ' in ' + geoLocation + ' and married to ' +
+        partner + ' ' + ' with ' + numKids + ' kids.');
+    }
+        
+        
+    var joshua = new tellFortune(children, partner, city, job);
+    joshua.fortune()
+    var hassan = new tellFortune(3, "Dania", "switzerland", "Senior developer");
+    hassan.fortune()
+    
