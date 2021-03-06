bf6e143271204d5bafa4f20b88046f831e7a55de
hassanwasswa <assanwasswa@gmail.com>
2021-03-06 18:02:42 +0100

    exe2 done

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -2,24 +2,15 @@
 // remember to always test in the browser
 // to see if everything works correctl
 
-       var jobTitle =prompt('Please enter your desired jobtitle');
-        var City =prompt('Please enter your desired location');
-        var partner =prompt('Please enter your partner ');
-        var numkids =prompt('please enter the numkids');
+function tellFortune() {
+    numKids = prompt("How many kids you want in a future");
+    spouseName = prompt("Name your future wife");
+    geoLoc = prompt("Where would like to live");
+    jobTitle = prompt("What would like to do as job"); 
+    return "You will be a " +jobTitle+ " in " +geoLoc+", and married to " +spouseName+ " with " +numKids+ " kids."
+}
 
-    function tellFortune(jobTitle, city, partner, numKids) {
-        this.jobTitle = jobTitle
-        this.geoLocation = city
-        this.partner = partner
-        this.numkids = numkids
-        this.fortune = function(){
-            window.alert('You will be a ' + jobTitle + ' in ' + geoLocation + ' and married to ' +
-        partner + ' ' + ' with ' + numKids + ' kids.');
-    }
-        
-        
-    var joshua = new tellFortune(children, partner, city, job);
-    joshua.fortune()
-    var hassan = new tellFortune(3, "Dania", "switzerland", "Senior developer");
-    hassan.fortune()
-    
+
+console.log(tellFortune())
+console.log(tellFortune())
+console.log(tellFortune())
